8146700c39d7c581774d368143f8b6ba139f4126
vishwanathsrikanth <vism@microsoft.com>
2018-10-19 00:07:04 +0530

    added more samples

---
+++ app/helloworld-nodejs-app/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World from Docker and Kubernetes with NodeJS Apps!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
